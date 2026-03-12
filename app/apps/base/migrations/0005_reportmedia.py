92e41f73f7265877293ac329f56b14ebfea64499
Nurbolottop <erk1nbaevw2711@gmail.com>
2026-03-12 20:50:17 +0600

    Add ReportMedia migration with correct dependency

---
+++ app/apps/base/migrations/0005_reportmedia.py
@@ -0,0 +1,27 @@
+# Generated manually for ReportMedia model
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('base', '0004_fcmdevicetoken'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ReportMedia',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('file', models.FileField(upload_to='reports/media/')),
+                ('media_type', models.CharField(choices=[('image', 'Фото'), ('video', 'Видео')], default='image', max_length=10)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('report', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='media_files', to='base.report')),
+            ],
+            options={
+                'verbose_name': 'Медиа файл отчёта',
+                'verbose_name_plural': 'Медиа файлы отчётов',
+            },
+        ),
+    ]
