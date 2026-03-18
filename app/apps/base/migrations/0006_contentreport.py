5688587825250e1388f94a5cb695b5b113d49faf
Nurbolottop <erk1nbaevw2711@gmail.com>
2026-03-19 04:59:04 +0600

    Add ContentReport API for reporting campaigns/organizations

--- app/apps/base/migrations/0006_contentreport.py
+++ app/apps/base/migrations/0006_contentreport.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('base', '0005_reportmedia'),
+        ('base', '0004_fcmdevicetoken'),
     ]
 
     operations = [
